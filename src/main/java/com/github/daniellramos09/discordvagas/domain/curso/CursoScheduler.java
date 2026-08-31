167951eefcb7853eda06116e43d86df82073c766
Daniel Expedito <danielexpeditospr2@gmail.com>
2026-08-31 19:31:18 -0300

    atualizações de segurança

--- src/main/java/com/github/daniellramos09/discordvagas/domain/curso/CursoScheduler.java
+++ src/main/java/com/github/daniellramos09/discordvagas/domain/curso/CursoScheduler.java
@@ -1,12 +1,14 @@
 package com.github.daniellramos09.discordvagas.domain.curso;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import static com.github.daniellramos09.discordvagas.controller.TestController.logger;
-
 @Component
 public class CursoScheduler {
+
+    private static final Logger logger = LoggerFactory.getLogger(CursoScheduler.class);
 
     private final CursoOrchestrator orchestrator;
 
--- src/main/java/com/github/daniellramos09/discordvagas/repository/VagaRepository.java
+++ src/main/java/com/github/daniellramos09/discordvagas/repository/VagaRepository.java
@@ -4,8 +4,6 @@ import com.github.daniellramos09.discordvagas.entity.Vaga;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface VagaRepository extends JpaRepository<Vaga, Long> {
 
